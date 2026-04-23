a6691bfd47c46c21348a5665a5165afcfdd4aab5
Pranav Kumar <pranavsingh7748@gmail.com>
2026-04-23 21:40:36 +0530

    create a class and  update

---
+++ src/condiational/ifelse.java
@@ -0,0 +1,18 @@
+package condiational;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int marks=67;
+        if(marks>90){
+            System.out.println("Excellent");
+        } else  if(marks>80 && marks<=90){
+            System.out.println("Good");
+        } else if (marks>70 && marks<80) {
+            System.out.println("Fair");
+        } else if (marks>60  && marks<70) {
+            System.out.println("Expectations");
+        } else if(marks<60){
+            System.out.println("Fair");
+        }
+    }
+}
