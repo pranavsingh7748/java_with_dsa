2485fc48b3bfe586c572f5754d471e99c524f250
Pranav Kumar <pranavsingh7748@gmail.com>
2026-05-11 20:47:02 +0530

    update class file

--- src/number_system/number_system4.java
+++ src/number_system/number_system4.java
@@ -1,4 +1,11 @@
 package number_system;
 
+import java.util.Scanner;
+
 public class number_system4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
 }
