45e886a37357e9b7c5db1d45848299ccf0647761
Pranav Kumar <pranavsingh7748@gmail.com>
2026-05-11 20:50:46 +0530

    update class file and create package

--- src/module_1/number_system5/number_system4.java
+++ src/module_1/number_system5/number_system4.java
@@ -6,6 +6,7 @@ public class number_system4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int b1 = sc.nextInt();
 
     }
 }
