8b82daf10900e8499c3925dafd5dd951aa094987
Pranav Kumar <pranavsingh7748@gmail.com>
2026-05-15 22:15:00 +0530

    update class file

---
+++ src/module_2/array_1/array5.java
@@ -0,0 +1,23 @@
+package module_2.array_1;
+import java.util.*;
+public class array5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int data = sc.nextInt();
+        int idx = -1;
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] == data){
+                idx = i;
+                break;
+            }
+        }
+        System.out.println(idx);
+    }
+}
