88b5b207d74c6f20e902f919c93200d5eac667d0
Pranav Kumar <pranavsingh7748@gmail.com>
2026-07-20 02:27:08 +0530

    Add UserAuthentication class

---
+++ src/module_3/CountVowelsAndConsonants.java
@@ -0,0 +1,34 @@
+package module_3;
+import java.util.*;
+public class CountVowelsAndConsonants {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < s.length(); i++){
+
+            char ch = Character.toLowerCase(s.charAt(i));
+
+            if (Character.isLetter(ch)){
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                    vowels++;
+                }
+
+                else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels : " + vowels);
+        System.out.println("Consonats : " + consonants);
+
+        sc.close();
+    }
+}
