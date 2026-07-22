5053ea2e1a5d158a819a9c2e1a83ea85c5ad4744
Pranav Kumar <pranavsingh7748@gmail.com>
2026-07-22 19:30:19 +0530

    Add UserAuthentication class

---
+++ src/module_3/lowercase_to_uppercase.java
@@ -0,0 +1,32 @@
+package module_3;
+import java.util.*;
+
+public class lowercase_to_uppercase {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int lowerCase = 0;
+        int upperCase = 0;
+
+        for (int i = 0; i < s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+
+                System.out.print(Character.toLowerCase(ch));
+
+            } else if (Character.isLetter(ch)) {
+                System.out.print(Character.toUpperCase(ch));
+
+            } else {
+                System.out.println(ch);
+            }
+        }
+
+        sc.close();
+     }
+}
