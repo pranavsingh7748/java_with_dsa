c41121250a7054c83b84f4ee28c0aaa620373f24
Pranav Kumar <pranavsingh7748@gmail.com>
2026-07-22 19:28:59 +0530

    Add UserAuthentication class

---
+++ src/module_3/Count_digits_and_special_characters.java
@@ -0,0 +1,36 @@
+package module_3;
+import java.util.*;
+
+public class Count_digits_and_special_characters {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+         int dig = 0;
+        int sp = 0;
+
+        for (int i = 0; i < s.length(); i++){
+
+            char ch= Character.toLowerCase(s.charAt(i));
+
+            if (Character.isDigit(ch)) {
+                dig++;
+            }
+
+            if (!Character.isLetter(ch)
+                    && !Character.isDigit(ch)
+                    && !Character.isWhitespace(ch)) {
+
+                sp++;
+            }
+
+        }
+        System.out.println("Digits : " + dig);
+        System.out.println("Special Characters : " + sp);
+
+        sc.close();
+    }
+}
