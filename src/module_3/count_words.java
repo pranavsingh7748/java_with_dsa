2e87b024f90488dc568a2e6afa2a684a69b6c9fa
Pranav Kumar <pranavsingh7748@gmail.com>
2026-07-22 20:18:07 +0530

    Add UserAuthentication class

---
+++ src/module_3/count_words.java
@@ -0,0 +1,21 @@
+package module_3;
+import java.util.*;
+
+public class count_words {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int spaceCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+        System.out.println(spaceCount + 1);
+    }
+}
